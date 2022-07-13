cef941f049706e10f2ed05bbfa91ddebbed8561b
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-07-13 18:08:21 +0100

    database schema and connection established

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 import express from 'express';
 import expressLayouts from 'express-ejs-layouts';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// checks to see if development dependencies are required or not
+if (process.env.NODE_ENV != "production") {
+    dotenv.config();
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 3000;
@@ -10,8 +22,22 @@ app.set("layout", "layouts/layout");
 
 app.use(expressLayouts);
 
-app.get("/", (req, res) => {
-    res.send("Hurray! It's working.");
-});
+// database connection
+mongoose
+    .connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        // listening for requests
+        app.listen(process.env.PORT || PORT);
+        console.log("Connected to Mongoose");
+    })
+    .catch((err) => {
+        console.log("Error connecting to Mongoose");
+        console.log(err);
+    });
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get("/", (req, res) => {
+    res.render("home");
+});
---
+++ models/campground.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const CampgroundSchema = new Schema(
+    {
+        title: String,
+        price: String,
+        description: String,
+        location: String
+    }
+);
+
+module.exports = mongoose.model("Campground", CampgroundSchema);
