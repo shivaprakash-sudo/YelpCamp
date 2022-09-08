501a71cea4670be181737c92d8a71cb31762f533
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-09-08 22:15:55 +0100

    add basic profile page

--- controllers/users.js
+++ controllers/users.js
@@ -1,44 +1,49 @@
 import catchAsync from "../utils/catchAsync.js";
-import User from '../models/user.js';
+import User from "../models/user.js";
 
 const signupForm = (req, res) => {
-    res.render('users/signup');
+  res.render("users/signup");
 };
 
 const createUser = catchAsync(async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', `Welcome to Yelp Camp, ${username}!`);
-            res.redirect('/campgrounds');
-        });
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('signup');
-    }
+  try {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash("success", `Welcome to Yelp Camp, ${username}!`);
+      res.redirect("/campgrounds");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("signup");
+  }
 });
 
 const loginForm = (req, res) => {
-    res.render('users/login');
+  res.render("users/login");
 };
 
 const login = (req, res) => {
-    req.flash('success', `Welcome back ${req.user.username} !`);
+  req.flash("success", `Welcome back ${req.user.username} !`);
 
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
+  const redirectUrl = req.session.returnTo || "/campgrounds";
+  delete req.session.returnTo;
+  res.redirect(redirectUrl);
 };
 
 const logout = (req, res, next) => {
-    req.logout((err) => {
-        if (err) return next(err);
-        req.flash('success', "Goodbye! Visit us again.");
-        res.redirect('/campgrounds');
-    });
+  req.logout((err) => {
+    if (err) return next(err);
+    req.flash("success", "Goodbye! Visit us again.");
+    res.redirect("/campgrounds");
+  });
 };
 
-export { signupForm, createUser, loginForm, login, logout };
+const profile = (req, res) => {
+  //   console.log(req.user);
+  res.render("users/profile", { user: req.user });
+};
+
+export { signupForm, createUser, loginForm, login, logout, profile };
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,32 @@
 import express from "express";
 import passport from "passport";
-import { signupForm, createUser, loginForm, login, logout } from "../controllers/users.js";
+import {
+  signupForm,
+  createUser,
+  loginForm,
+  login,
+  logout,
+  profile,
+} from "../controllers/users.js";
 
 const router = express.Router();
 
-router.route('/signup')
-    .get(signupForm)
-    .post(createUser);
+router.route("/signup").get(signupForm).post(createUser);
 
-router.route('/login')
-    .get(loginForm)
-    .post(passport.authenticate('local', {
-        failureFlash: true,
-        failureRedirect: '/login',
-        keepSessionInfo: true
-    }), login);
+router
+  .route("/login")
+  .get(loginForm)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+      keepSessionInfo: true,
+    }),
+    login
+  );
 
-router.get('/logout', logout);
+router.get("/logout", logout);
+
+router.get("/profile", profile);
 
 export default router;
